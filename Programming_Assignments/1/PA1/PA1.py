bfa2c95f11a171de04edbb20763b72007317ee35
Danny Throop <DanielThroop123@gmail.com>
2021-02-06 17:45:01 -0600

    Homework 2 work

---
+++ Programming_Assignments/1/PA1/PA1.py
@@ -0,0 +1,134 @@
+"""
+PA1.py - analyzing the runtimes of the python fibonnaci implementation for both recursive and non-recursive solutions
+Date - 11/18/20 
+Author - Daniel Throop
+Class - COSC 3100
+
+"""
+	''
+import time
+import math
+import xlsxwriter
+
+# intialize workbooks
+outWorkbook = xlsxwriter.Workbook("out.xlsx")
+recFibSheet = outWorkbook.add_worksheet()
+goodFibSheet = outWorkbook.add_worksheet()
+
+# writing headers
+recFibSheet.write(0, 0, "rec_fib(n)")
+goodFibSheet.write(0, 0, "good_fib(n)")
+
+# intialzing of both vertical and horizontal colums in excel
+for i in range(1, 25):
+	recFibSheet.write(0, i, i)
+	recFibSheet.write(i, 0, i)
+	goodFibSheet.write(0, i, i)
+	goodFibSheet.write(i, 0, i)
+
+# intializes dictionary to gather durations of fib() runs
+def initDict(n):
+	dummy = {}
+	for i in range(1, n):
+		dummy[i] = []
+	return dummy
+
+durations = initDict(25)
+averages = initDict(25)
+
+# recursive fibonacci
+def rec_fibonnaci(i):
+	if i < 2:
+		return i
+	else:
+		return rec_fibonnaci(i-1)+rec_fibonnaci(i-2)
+
+# non-recursive fibonacci
+def good_fibonnaci(i):
+	pre = 0
+	curr = 1
+	if i == 0:
+		return 0
+	elif i == 1:
+		return 1
+	else:
+		for _ in range(i - 1):
+			curr, pre = curr+pre, curr
+		return curr
+
+# recursive fibonacci trials - 24 total
+for trial in range(1, 25):
+
+	# recording time durations (nsecs)
+	for i in range(1, 25):
+		total = 0
+		print(i)
+		for j  in range(20):
+			start_time = time.perf_counter_ns()
+			for _ in range(50):
+				x = rec_fibonnaci(i)
+			duration = (time.perf_counter_ns() - start_time)/50
+			print("{:12.10f}".format(duration))
+			durations[i].append(duration)
+
+		# storing the average of the 20 runs from fib(n)
+
+		# sum the 20 runs of fib(n)
+		for j in range(len(durations[1])):
+			total = total + durations[i][j]
+		# take the average of the total
+		total = total/20.0
+		# store away average to be put in excel
+		averages[i] = round(total, 4 - (int(math.floor(math.log10(abs(total)))) - 1))
+		print("\n")
+
+	# write averages to excel sheet
+	for row in range(1, len(averages) + 1):
+		recFibSheet.write(row, trial, str(averages[row]))
+
+	# clear and re-intialize data-structures
+	durations.clear()
+	averages.clear()
+	durations = initDict(25)
+	averages = initDict(25)
+
+# non-recursive fibonacci trials - 24 total
+for trial in range(1, 25):
+	# recording time durations (nsecs)
+	for i in range(1, 25):
+		total = 0
+		print(i)
+		for j  in range(20):
+			start_time = time.perf_counter_ns()
+			for _ in range(50):
+				x = good_fibonnaci(i)
+			duration = (time.perf_counter_ns() - start_time)/50
+			print("{:12.10f}".format(duration))
+			durations[i].append(duration)
+
+		# storing the average of the 20 runs from fib(n)
+
+		# sum the 20 runs of fib(n)
+		for j in range(len(durations[1])):
+			total = total + durations[i][j]
+		# take the average of the total
+		total = total/20.0
+		# store away average to be put in excel
+		averages[i] = round(total, 4 - (int(math.floor(math.log10(abs(total)))) - 1))
+		print("\n")
+
+	# write averages to excel sheet
+	for row in range(1, len(averages) + 1):
+		goodFibSheet.write(row, trial, str(averages[row]))
+
+	# clear and re-intialize data-structures
+	durations.clear()
+	averages.clear()
+	durations = initDict(25)
+	averages = initDict(25)
+
+# closing workbook
+outWorkbook.close()
+
+
+
---
+++ Programming_Assignments/2/PA2_1.py
@@ -0,0 +1,36 @@
+"""
+PA2_1.py - The first problem in programming assigment 2. Given a 64 bit number determine the number of ones in the binary representation of the number. 
+Date - 02/06/21 
+Author - Daniel Throop
+Class - COSC 3100
+"""
+# returns the number of one bits in a decimal number n
+# def countOnes(n):
+# 	total = 0
+# 	binary = bin(n)
+# 	binary = binary[2:len(binary)]
+
+# 	for i in range(0, len(binary)):
+# 		if(int(binary[i]) ==  1):
+# 			total += 1
+
+# 	return total
+
+table = {0: 0, 1: 1, 2: 1, 3: 2, 4: 1, 5: 2, 6: 2, 7: 3, 8: 1, 9: 2, 0xa: 2, 0xb: 3, 0xc: 2, 0xd: 3, 0xe: 3, 0xf: 4}
+
+# function that uses bitwise operations to determine number of 1 bits in number
+def countOnes(n):
+	total = 0
+
+	while(n != 0):
+		#mask first four bits
+		temp = n & 0xf
+		#lookup num of ones and add to total
+		total += table[temp]
+		#shift binary right by 4 bits
+		n = n >> 4 
+
+	return total
+
+
+
