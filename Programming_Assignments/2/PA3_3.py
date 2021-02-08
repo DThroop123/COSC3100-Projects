b6c419992b03e81c586ba28c50ebed6a736b96ef
Danny Throop <DanielThroop123@gmail.com>
2021-02-08 15:54:30 -0600

    homework 2 first submission

--- Programming_Assignments/2/PA3_3.py
+++ Programming_Assignments/2/PA3_3.py
@@ -43,59 +43,172 @@ def checkSol(board):
 		# reset the table to all zeros 
 		table = reset(table)
 
-	# check houses (3x3 subgrid)
+	# check house 1 (3x3 subgrid)
 
-	for col in range(3, 9, 3):
-		for i in range(col):
-			for j in range(3):
-				currNum = board[i][j]
-					# check if currNum has already been seen
-				if(currNum == 0):
-					pass
+	for i in range(3):
+		for j in range(3):
+			currNum = board[i][j]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
 				else:
-					if(table[currNum - 1] & 1):
-						return False
-					else:
-						# show that we have seen the number 
-						table[currNum - 1] = 1 
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
 			
-		# reset table to all zeros					
-		table = reset(table)
+	# reset table to all zeros					
+	table = reset(table)
 
-	for col in range(3, 9, 3):
-		for i in range(col):
-			for j in range(3,6,1):
-				currNum = board[i][j]
-					# check if currNum has already been seen
-				if(currNum == 0):
-					pass
+	# check house 2 (3x3 subgrid)
+
+	for i in range(3,6,1):
+		for j in range(3):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
 				else:
-					if(table[currNum - 1] & 1):
-						return False
-					else:
-						# show that we have seen the number 
-						table[currNum - 1] = 1 
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
 			
-		# reset table to all zeros					
-		table = reset(table)
+	# reset table to all zeros					
+	table = reset(table)
 
-	for col in range(3, 9, 3):
-		for i in range(col):
-			for j in range(6,8,1):
-				currNum = board[i][j]
-					# check if currNum has already been seen
-				if(currNum == 0):
-					pass
+	# check house 3 (3x3 subgrid)
+
+	for i in range(6,9,1):
+		for j in range(3):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
 				else:
-					if(table[currNum - 1] & 1):
-						return False
-					else:
-						# show that we have seen the number 
-						table[currNum - 1] = 1 
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
 			
-		# reset table to all zeros					
-		table = reset(table)
+	# reset table to all zeros					
+	table = reset(table)
 
+	# check house 4 (3x3 subgrid)
+
+	for i in range(3):
+		for j in range(3,6,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+
+	# check house 5 (3x3 subgrid)
+
+	for i in range(3,6,1):
+		for j in range(3,6,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+	# check house 6 (3x3 subgrid)
+
+	for i in range(6,9,1):
+		for j in range(3,6,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+
+	# check house 7 (3x3 subgrid)
+
+	for i in range(3):
+		for j in range(6,9,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+
+	# check house 8 (3x3 subgrid)
+
+	for i in range(3,6,1):
+		for j in range(6,9,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+		# check house 8 (3x3 subgrid)
+
+	for i in range(6,9,1):
+		for j in range(6,9,1):
+			currNum = board[j][i]
+				# check if currNum has already been seen
+			if(currNum == 0):
+				pass
+			else:
+				if(table[currNum - 1] == 1):
+					return False
+				else:
+					# show that we have seen the number 
+					table[currNum - 1] = 1 
+			
+	# reset table to all zeros					
+	table = reset(table)
+
+	#partial solution is valid
 	return True
 
 # function that resets the lookup table
@@ -108,8 +221,15 @@ def reset(table):
 	return clean
 
 
-testBoard = [[0,5,0,0,0,8,0,4,0], [0,4,0,3,0,0,0,7,0], [0,3,1,7,2,0,8,9,0],
-[3,0,0,0,0,0,7,8,0], [0,0,5,0,0,0,1,0,0], [0,6,2,0,0,0,0,0,3], [0,2,6,0,4,7,9,3,0], [0,8,0,0,0,6,0,2,0], [0,9,0,8,0,0,0,1,0]]
+testBoard = [[0,5,0,0,0,8,0,4,0], 
+			 [0,4,0,3,0,0,0,7,0], 
+			 [0,3,1,7,2,0,8,9,0],
+			 [3,0,0,0,0,0,7,8,0], 
+             [0,0,5,0,0,0,1,0,0], 
+             [0,6,2,0,0,0,0,0,3], 
+             [0,2,6,0,4,7,9,3,0], 
+             [0,8,0,0,0,6,0,2,0], 
+             [0,9,0,8,0,0,0,1,0]]
 
 print(checkSol(testBoard))
 
